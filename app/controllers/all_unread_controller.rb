f3164dc8c80c053aa31a1a53465d511c2a45b7fc
CMmZLN <zawlinnaing.cybermissions.mm@gmail.com>
2024-05-03 11:51:47 +0630

    edit allUnread controller

--- app/controllers/all_unread_controller.rb
+++ app/controllers/all_unread_controller.rb
@@ -18,14 +18,14 @@ class AllUnreadController < ApplicationController
     #Select unread group messages from mysql database        
     @temp_user_channelids=TUserChannel.select("unread_channel_message").where("message_count > 0 and userid=?",params[:user_id])
 
-    
+    @tmp_user_channelids=[]
     @t_user_channelids = Array.new
     @temp_user_channelids.each do |u_channel|
         unless u_channel.unread_channel_message.nil?
-            @t_user_channelids = u_channel.unread_channel_message.split(",")
+            @tmp_user_channelids << u_channel.unread_channel_message.split(",") unless u_channel.unread_channel_message.nil?
         end
     end
-
+    @t_user_channelids = @tmp_user_channelids.flatten
 
     @t_group_messages = TGroupMessage.select("t_group_messages.id,t_group_messages.groupmsg,t_group_messages.created_at,m_users.name,m_channels.channel_name, (select count(*) from t_group_threads where t_group_threads.t_group_message_id = t_group_messages.id) as count, m_channels.channel_name")
     .joins("INNER JOIN m_users ON m_users.id = t_group_messages.m_user_id
